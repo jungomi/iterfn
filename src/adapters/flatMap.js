cc1f7f1db67b114a30e95ec5c67086b754ec8f9e
Michael Jungo <michaeljungo92@gmail.com>
2017-01-29 21:16:13 +0100

    Change the semantics of flatMap
    flatMap is supposed to flatten iterators that are returned from the map
    function, as opposed to flatten any values that are iterable before
    giving it to the map function. That means that mapFunc returns an
    iterator instead of a value.

--- src/adapters/flatMap.js
+++ src/adapters/flatMap.js
@@ -1,12 +1,6 @@
-import { isIterable } from './../utils';
-
 function* flatMap(iterable, mapFunc) {
   for (const val of iterable) {
-    if (isIterable(val)) {
-      yield* flatMap(val, mapFunc);
-    } else {
-      yield mapFunc(val);
-    }
+    yield* mapFunc(val);
   }
 }
 
--- test/adapters/flatMap.test.js
+++ test/adapters/flatMap.test.js
@@ -1,13 +1,39 @@
 import test from 'ava';
 import flatMap from '../../src/adapters/flatMap';
 
-test('flatMap iterates over nested iterators', t => {
-  const iter = flatMap([[1, [2, 3]], 4], x => x * 2);
+test('flatMap iterates over nested iterators when returning an array', t => {
+  const iter = flatMap([1, 2, 3], x => {
+    const result = [];
+    for (let i = 0; i < x; i++) {
+      result.push(x);
+    }
+    return result;
+  });
 
   t.is(typeof iter.next, 'function');
+  t.is(iter.next().value, 1);
   t.is(iter.next().value, 2);
-  t.is(iter.next().value, 4);
-  t.is(iter.next().value, 6);
-  t.is(iter.next().value, 8);
+  t.is(iter.next().value, 2);
+  t.is(iter.next().value, 3);
+  t.is(iter.next().value, 3);
+  t.is(iter.next().value, 3);
+  t.true(iter.next().done);
+});
+
+test('flatMap iterates over nested iterators when returning an iterator', t => {
+  function* innerIter(x) {
+    for (let i = 0; i < x; i++) {
+      yield x;
+    }
+  }
+  const iter = flatMap([1, 2, 3], x => innerIter(x));
+
+  t.is(typeof iter.next, 'function');
+  t.is(iter.next().value, 1);
+  t.is(iter.next().value, 2);
+  t.is(iter.next().value, 2);
+  t.is(iter.next().value, 3);
+  t.is(iter.next().value, 3);
+  t.is(iter.next().value, 3);
   t.true(iter.next().done);
 });
