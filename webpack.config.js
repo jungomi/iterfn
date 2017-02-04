3e979751dce4cc7bfbe541fc367c338511418472
Michael Jungo <michaeljungo92@gmail.com>
2017-02-04 19:57:29 +0100

    Add umd build

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'iter.min.js',
+    library: 'Iter',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    })
+  ],
+  stats: {
+    colors: true
+  }
+};
+
+module.exports = config;
